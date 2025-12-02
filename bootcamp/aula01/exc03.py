9da0a6e7aa08e66fa31afd1c354230bc5dccc3a6
Lipexh <felipeh25@outlook.com>
2025-12-02 16:06:30 -0300

    Adicionando exc 03

---
+++ bootcamp/aula01/exc03.py
@@ -0,0 +1,14 @@
+## Exercício 03: Refatore o exercício 02 atribuindo variáveis
+
+## print(len(input("Digite o seu nome: ")))
+
+nome = (input("Digite seu nome "))
+idade = (input("Digite sua idade "))
+
+quantidade_nome = len(nome)
+quantidade_idade = len(idade)
+
+print("A quantidade de letras do nome é: ", quantidade_nome, "a quantidade de letras da idade é: ", quantidade_idade)
+print("E a somatória dos dois é: ", quantidade_idade + quantidade_nome)
+
+
